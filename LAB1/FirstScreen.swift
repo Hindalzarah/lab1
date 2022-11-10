bc76ad2c53a41052490cfb884d3d43199a27f64f
Hind <hindalzarah@outlook.com>
2022-11-10 09:44:40 +0300

    first v

---
+++ LAB1/FirstScreen.swift
@@ -0,0 +1,22 @@
+//
+//  FirstScreen.swift
+//  LAB1
+//
+//  Created by Hind on 01/04/1444 AH.
+//
+
+import SwiftUI
+
+struct FirstScreen: View {
+    var body: some View {
+        Text("")
+        
+    }
+    }
+
+
+struct FirstScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        FirstScreen()
+    }
+}
---
+++ LAB1/LAB1App.swift
@@ -0,0 +1,17 @@
+//
+//  LAB1App.swift
+//  LAB1
+//
+//  Created by Hind on 01/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct LAB1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
